f406ba7fe6b7b708098cc291addfdedf583de004
kotaro <kotaro@KotaronoMacBook-Pro.local>
2022-08-09 14:12:59 +0900

    Changed the HTML for the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GOOGLE_ANALYTICS_PROPERTY_ID = 'G-SK77EKLV8H'
